85457434fc6082776fdaf8e9e1f1720f26343210
Jenking-Zhang <zhangjunjing@live.com>
2017-12-14 15:55:08 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -17,24 +17,24 @@ export MBEDTLS_LINK="https://tls.mbed.org/download/mbedtls-${MBEDTLS_VER}-gpl.tg
 #old export SS_LIBEV_VER=$(curl -L -s https://github.com/shadowsocks/shadowsocks-libev/releases/latest | grep "/shadowsocks/shadowsocks-libev/releases/download/" | cut -f4 -d \- | awk -F ".tar" '{print $1}' | sed s/[[:space:]]//g )
 export SS_LIBEV_VER=$(curl -L -s https://github.com/shadowsocks/shadowsocks-libev/releases/latest | grep "/shadowsocks/shadowsocks-libev/releases/download/" |head -n 1 |cut -f6 -d "/" | awk -F "v" '{print $1,$2}' | sed s/[[:space:]]//g )
 export SS_LIBEV_LINK="https://github.com/shadowsocks/shadowsocks-libev/releases/download/v${SS_LIBEV_VER}/shadowsocks-libev-${SS_LIBEV_VER}.tar.gz"
-export SS_LIBEV_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell/master/ss_ssr_kcp_bbr_netspeeder/ss_libev.init"
+export SS_LIBEV_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/ss_libev.init"
 export SS_LIBEV_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ss_libev_apt.init"
 
 # SSR
 #export SSR_VER=3.4.0
 export SSR_VER=$(wget --no-check-certificate -qO- https://raw.githubusercontent.com/onekeyshell/shadowsocksr/manyuser/shadowsocks/version.py | grep return | cut -d\' -f2 | awk '{print $1}' | sed s/[[:space:]]//g )
 export SSR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/manyuser.zip"
-export SSR_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell/master/ss_ssr_kcp_bbr_netspeeder/ssr.init"
+export SSR_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/ssr.init"
 export SSR_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssr_apt.init"
 # SSRR
 #export SSRR_VER=3.2.1
 export SSRR_VER=$(wget --no-check-certificate -qO- https://raw.githubusercontent.com/shadowsocksrr/shadowsocksr/akkariiin/dev/shadowsocks/version.py | grep return | cut -d\' -f2 | awk '{print $2}')
 export SSRR_LINK="https://github.com/shadowsocksrr/shadowsocksr/archive/akkariiin/master.zip"
-export SSRR_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell/master/ss_ssr_kcp_bbr_netspeeder/ssrr.init"
+export SSRR_YUM_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/ssrr.init"
 export SSRR_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssrr_apt.init"
 # KCPTUN
 #export KCPTUN_VER=20171201
 export KCPTUN_VER=$(curl -L -s https://github.com/xtaci/kcptun/releases/latest | grep "/xtaci/kcptun/releases/download/" |head -n 1 |cut -f6 -d "/" | awk -F "v" '{print $1,$2}' | sed s/[[:space:]]//g )
 export KCPTUN_AMD64_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz"
 export KCPTUN_386_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-386-${KCPTUN_VER}.tar.gz"
-export KCPTUN_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell/master/ss_ssr_kcp_bbr_netspeeder/kcptun.init"
+export KCPTUN_INIT="https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/kcptun.init"
