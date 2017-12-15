7cd3aa46f81a559c6979580f6fbee3dbd07fc500
Jenking-Zhang <zhangjunjing@live.com>
2017-12-15 09:04:06 +0800

    Update get-rinetd-bbr.sh

--- get-rinetd-bbr.sh
+++ get-rinetd-bbr.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
-# Usage:
-#   curl https://raw.githubusercontent.com/linhua55/lkl_study/master/get-rinetd.sh | bash
+#! /bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
+cur_dir=$(pwd)
+remote_bbr_version=$(wget --no-check-certificate -qO- https://api.github.com/repos/linhua55/lkl_study/releases/latest | grep 'tag_name' | cut -d\" -f4)
+RINET_BBR_URL="https://github.com/linhua55/lkl_study/releases/download/${remote_bbr_version}/rinetd_bbr_powered"
 
-BBR_VER=$(wget --no-check-certificate -qO- https://api.github.com/repos/linhua55/lkl_study/releases/latest | grep 'tag_name' | cut -d\" -f4)
-RINET_BBR_URL="https://github.com/linhua55/lkl_study/releases/download/${BBR_VER}/rinetd_bbr_powered"
-export BBR_INIT_URL="https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/bbr.init"
-
-
+COLOR_RED='\E[1;31m'
+COLOR_GREEN='\E[1;32m'
+COLOR_YELOW='\E[1;33m'
+COLOR_BLUE='\E[1;34m'
+COLOR_PINK='\E[1;35m'
+COLOR_PINKBACK_WHITEFONT='\033[45;37m'
+COLOR_GREEN_LIGHTNING='\033[32m \033[05m'
+COLOR_END='\E[0m'
 
 if [ "$(id -u)" != "0" ]; then
-    echo "ERROR: Please run as root"
+    echo -e "${COLOR_RED}ERROR: You must be root to run this script!${COLOR_END}"
     exit 1
 fi
 
@@ -23,19 +29,21 @@ fi
 #	fi
 #done
 
-echo "1. Download rinetd-bbr from $RINET_BBR_URL"
-curl -L "${RINET_BBR_URL}" >/usr/bin/rinetd-bbr
-chmod +x /usr/bin/rinetd-bbr
+echo -e "Get the Rinetd-BBR version:${COLOR_GREEN}${remote_bbr_version}${COLOR_END}"
+echo " Download Rinetd-BBR from $RINET_BBR_URL"
+curl -L "${RINET_BBR_URL}" >/usr/bin/Rinetd-BBR
+chmod +x /usr/bin/Rinetd-BBR
 
-echo "2. Generate /etc/rinetd-bbr/bbr.conf"
-mkdir /etc/rinetd-bbr/
-cat <<EOF > /etc/rinetd-bbr/bbr.conf
-#version="${BBR_VER}"
+echo "Config Rinetd-BBR..."
+[ ! -d /etc/Rinetd-BBR/ ] && mkdir /etc/Rinetd-BBR/
+[ -d /etc/Rinetd-BBR/bbr.conf ] && rm -rf /etc/Rinetd-BBR/bbr.conf
+cat <<EOF > /etc/Rinetd-BBR/bbr.conf
+#bbr_version="${remote_bbr_version}"
 # bindadress bindport connectaddress connectport
 0.0.0.0 443 0.0.0.0 443
 EOF
 
-echo "3. Config service"
+echo "Config service..."
 wget --no-check-certificate "${BBR_INIT_URL}" -O /etc/init.d/bbr
 chmod +x /etc/init.d/bbr
 chkconfig --add bbr
