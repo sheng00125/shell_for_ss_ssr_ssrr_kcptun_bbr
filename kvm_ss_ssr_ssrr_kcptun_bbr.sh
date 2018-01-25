69c67bf075883a719bccbe5729842b3300a4bba3
Jenking-Zhang <zhangjunjing@live.com>
2018-01-25 18:55:06 +0800

    Update kvm_ss_ssr_ssrr_kcptun_bbr.sh

--- kvm_ss_ssr_ssrr_kcptun_bbr.sh
+++ kvm_ss_ssr_ssrr_kcptun_bbr.sh
@@ -2003,7 +2003,7 @@ reconfig_ss_ssr_ssrr_kcptun(){
 }
 set_tool(){
     echo -e "${COLOR_YELOW}set tool...${COLOR_END}"
-    wget --no-check-certificate https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/tool.sh
+    wget --no-check-certificate -O /root/tool.sh https://raw.githubusercontent.com/Jenking-Zhang/shell_for_ss_ssr_ssrr_kcptun_bbr/master/kvm_tool.sh
     chmod +x /root/tool.sh
 }
 update_ss_ssr_ssrr_kcptun(){
