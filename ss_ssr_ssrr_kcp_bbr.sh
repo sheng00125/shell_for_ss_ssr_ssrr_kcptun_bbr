b1fd173bf4a43f7cbde72d9e56e1fe360b397040
Jenking-Zhang <zhangjunjing@live.com>
2018-06-10 11:31:06 +0800

    Update ss_ssr_ssrr_kcp_bbr.sh

--- ss_ssr_ssrr_kcp_bbr.sh
+++ ss_ssr_ssrr_kcp_bbr.sh
@@ -743,8 +743,9 @@ config_ss_ssr_ssrr_kcptun(){
     else
         if [ "${bbr_install}" == "y" ] || [ "${bbr_install}" == "Y" ]; then
             fast_open="true"
+	else
+	    fast_open="false"
 	fi
-	fast_open="false"
     fi
     if [[ "${ss_libev_installed_flag}" == "false" && "${shell_action}" =~ ^[Ii]|[Ii][Nn]|[Ii][Nn][Ss][Tt][Aa][Ll][Ll]|-[Ii]|--[Ii]$ ]]; then
         [ ! -d /etc/shadowsocks-libev ] && mkdir -p /etc/shadowsocks-libev
